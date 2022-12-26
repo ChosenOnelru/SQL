99a38fc9ec498233424d8c8c80e76aea638d5ca6
Admin <chosenonex1@gmail.com>
2022-12-26 17:41:50 +0600

    SQL

--- Nested queries/1.sql
+++ Nested queries/1.sql
@@ -20,4 +20,4 @@ SELECT DISTINCT Product.maker, Printer.price
 FROM Printer JOIN Product ON Printer.model = Product.model
 WHERE Printer.price = (SELECT MIN(Printer.price) 
  FROM Printer  WHERE Printer.color = 'y')
-AND color ='y'
+AND color ='y' 
--- Nested queries/2.sql
+++ Nested queries/2.sql
@@ -1,3 +1,3 @@
 1. Найти все покупки в стране Country 6
-
+ 
 SELECT * FROM sale WHERE store_id IN (SELECT store_id FROM store WHERE city_id IN (SELECT city_id FROM city WHERE country_id IN (SELECT country_id FROM country WHERE country_name='Country 6')))
